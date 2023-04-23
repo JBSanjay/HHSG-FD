261ba294e3af84d26f7cd80f05b12b1aa9728651
hari1538radha <hari.r2020ece@sece.ac.in>
2023-04-23 07:13:08 +0530

    feat : mongodb connection made

--- server/server.js
+++ server/server.js
@@ -1,16 +1,25 @@
-import express from 'express';
-
-import mongoose from 'mongoose';
+import express from "express";
+import dummy from "./schema/dummy.js";
+import mongoose from "mongoose";
 // var Teewt = require('mongoose').model('Teewt');
-import user from './schema/UserSchema.js';
+import user from "./schema/UserSchema.js";
 
-const app=express();
+const app = express();
 
-app.get('/hello',(req,res)=>{
-    res.send("Hello This is sanjay")
-})
+app.get("/hello", (req, res) => {
+  res.send(dummy);
+});
 
-mongoose.connect('mongodb://localhost:27017/Library-Login');
-app.listen(4000,()=>{
-    console.log("listening at port 4000")
-})
+app.listen(4000, () => {
+  const mongoUrl = "mongodb://localhost:27017/Library-Login";
+  mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error: "));
+  db.once("open", function () {
+    console.log("connected")
+  });
+  console.log("listening at port 4000");
+});
